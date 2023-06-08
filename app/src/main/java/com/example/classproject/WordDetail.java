d5a690ffff7bdfbe33a74e8b14071bbb12f7b861
kurukuruzz <112538549+kurukuruzz@users.noreply.github.com>
2023-06-08 16:53:06 +0900

    SQL 연결 및 데이터베이스 조회 기능 추가, 연결되지 않은 화면 연결 및 TextView 추가

--- app/src/main/java/com/example/classproject/WordDetail.java
+++ app/src/main/java/com/example/classproject/WordDetail.java
@@ -1,40 +1,59 @@
 package com.example.classproject;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import org.json.JSONObject;
 
-public class WordDetail extends AppCompatActivity implements View.OnClickListener{
-    private Button backBtn, Btn;
+public class WordDetail extends AppCompatActivity implements View.OnClickListener {
+    private Button backBtn, practiceBtn;
+    private TextView tvData, wData;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_word);
 
-        backBtn = (Button) findViewById(R.id.backBtn);
-        Btn = (Button) findViewById(R.id.Btn);
+        int buttonId = getIntent().getIntExtra("buttonId", 0);
+
+        backBtn = findViewById(R.id.backBtn);
+        practiceBtn = findViewById(R.id.Btn);
+        tvData = findViewById(R.id.textView);
+        wData = findViewById(R.id.wordView);
 
         backBtn.setOnClickListener(this);
-        Btn.setOnClickListener(this);
+        practiceBtn.setOnClickListener(this);
+
+        wData.setText(getIntent().getStringExtra("word"));
+        tvData.setText(getIntent().getStringExtra("detail"));
     }
 
     @Override
     public void onClick(View v) {
+        int buttonId = getIntent().getIntExtra("buttonId", 0);
+        String word = getIntent().getStringExtra("word");
+        String detail = getIntent().getStringExtra("detail");
+        String voice = getIntent().getStringExtra("voice");
+
+        Intent intent;
         switch (v.getId()) {
             case R.id.backBtn:
-                Intent intent1 = new Intent(getApplicationContext(), WordListActivity.class);
-                startActivity(intent1);
+                intent = new Intent(this, WordListActivity.class);
                 break;
             case R.id.Btn:
-                Intent intent2 = new Intent(getApplicationContext(), WordPractice.class);
-                startActivity(intent2);
+                intent = new Intent(this, WordPractice.class);
+                intent.putExtra("buttonId", buttonId);
+                intent.putExtra("word", word);
+                intent.putExtra("detail", detail);
+                intent.putExtra("voice", voice);
                 break;
+            default:
+                return;
         }
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/classproject/WordListActivity.java
+++ app/src/main/java/com/example/classproject/WordListActivity.java
@@ -1,35 +1,98 @@
 package com.example.classproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import java.io.IOException;
+import java.util.Arrays;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WordListActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button wordBtn[] = new Button[10];
+    private Button[] wordBtn = new Button[10];
+    private Button backBtn;
     private Integer[] btnId = {
             R.id.wordBtn1, R.id.wordBtn2, R.id.wordBtn3, R.id.wordBtn4, R.id.wordBtn5,
             R.id.wordBtn6, R.id.wordBtn7, R.id.wordBtn8, R.id.wordBtn9, R.id.wordBtn10
     };
+    private static final String TAG = "ButtonOnClick";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_word);
 
-        for(int i = 0; i < wordBtn.length; i++) {
-            wordBtn[i] = (Button) findViewById(btnId[i]);
-        }
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(this);
 
-        for(int i = 0; i < wordBtn.length; i++) {
-            final int j = i;
-            //wordBtn[j].setOnClickListener(this);
+        for (int i = 0; i < wordBtn.length; i++) {
+            wordBtn[i] = findViewById(btnId[i]);
+            wordBtn[i].setOnClickListener(this);
         }
     }
 
     @Override
     public void onClick(View v) {
-        //버튼별 db 데이터 처리 화면 연결
+        int buttonId = v.getId();
+        int index = Arrays.asList(btnId).indexOf(buttonId);
+        if (index != -1) {
+            String url = "http://192.168.64.2:3000/btn" + (index + 1);
+            String name = wordBtn[index].getText().toString();
+            handleButtonClick(url, name, buttonId);
+        } else if (buttonId == R.id.backBtn) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void handleButtonClick(String url, String name, int buttonId) {
+        new Thread(() -> {
+            try {
+                OkHttpClient client = new OkHttpClient();
+                String requestBody = "{\"user_id\": \"" + url + "\", \"name\": \"" + name + "\"}";
+                RequestBody body = RequestBody.create(MediaType.parse("application/json"), requestBody);
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(body)
+                        .build();
+                Response response = client.newCall(request).execute();
+                if (response.isSuccessful()) {
+                    final String responseData = response.body().string();
+                    runOnUiThread(() -> {
+                        Log.d(TAG, url);
+                        try {
+                            String word = new JSONObject(responseData).getString("word");
+                            String voice = new JSONObject(responseData).getString("voice");
+                            String detail = new JSONObject(responseData).getString("detail");
+                            String videoUrl = new JSONObject(responseData).getString("videoUrl");
+
+                            Log.d(TAG, videoUrl);
+                            Intent intent = new Intent(WordListActivity.this, WordDetail.class);
+                            intent.putExtra("buttonId", buttonId);
+                            intent.putExtra("word", word);
+                            intent.putExtra("detail", detail);
+                            intent.putExtra("voice", voice);
+                            intent.putExtra("videoData", videoUrl);
+                            startActivity(intent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                } else {
+                    Log.d(TAG, "연결 안됨");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
