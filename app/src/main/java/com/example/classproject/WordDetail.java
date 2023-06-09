9bdab8629bb32d11c1e03dbf940e36f7bbf31d94
kurukuruzz <112538549+kurukuruzz@users.noreply.github.com>
2023-06-09 12:38:02 +0900

    json파일 받아오기 수정

--- app/src/main/java/com/example/classproject/WordDetail.java
+++ app/src/main/java/com/example/classproject/WordDetail.java
@@ -2,15 +2,22 @@ package com.example.classproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.VideoView;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class WordDetail extends AppCompatActivity implements View.OnClickListener {
     private Button backBtn, practiceBtn;
     private TextView tvData, wData;
+    private VideoView videoData;
+    private static final String TAG = "ButtonOnClick";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +25,27 @@ public class WordDetail extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.detail_word);
 
         int buttonId = getIntent().getIntExtra("buttonId", 0);
+        String pronunciationUrl = getIntent().getStringExtra("pronunciation_url");
+        Log.d(TAG, pronunciationUrl);
+        Log.d(TAG, pronunciationUrl);
+        Log.d(TAG, pronunciationUrl);
 
         backBtn = findViewById(R.id.backBtn);
         practiceBtn = findViewById(R.id.Btn);
         tvData = findViewById(R.id.textView);
         wData = findViewById(R.id.wordView);
+        videoData = findViewById(R.id.videoView);
 
         backBtn.setOnClickListener(this);
         practiceBtn.setOnClickListener(this);
 
         wData.setText(getIntent().getStringExtra("word"));
         tvData.setText(getIntent().getStringExtra("detail"));
+
+        videoData.setVideoPath(pronunciationUrl);
+        videoData.start();
     }
+
 
     @Override
     public void onClick(View v) {
--- app/src/main/java/com/example/classproject/WordListActivity.java
+++ app/src/main/java/com/example/classproject/WordListActivity.java
@@ -12,12 +12,14 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import java.io.IOException;
 import java.util.Arrays;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class WordListActivity extends AppCompatActivity implements View.OnClickListener {
+public class WordListActivity extends AppCompatActivity {
     private Button[] wordBtn = new Button[10];
     private Button backBtn;
     private Integer[] btnId = {
@@ -32,27 +34,29 @@ public class WordListActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.list_word);
 
         backBtn = findViewById(R.id.backBtn);
-        backBtn.setOnClickListener(this);
+        backBtn.setOnClickListener(click);
 
         for (int i = 0; i < wordBtn.length; i++) {
             wordBtn[i] = findViewById(btnId[i]);
-            wordBtn[i].setOnClickListener(this);
+            wordBtn[i].setOnClickListener(click);
         }
     }
 
-    @Override
-    public void onClick(View v) {
-        int buttonId = v.getId();
-        int index = Arrays.asList(btnId).indexOf(buttonId);
-        if (index != -1) {
-            String url = "http://192.168.64.2:3000/btn" + (index + 1);
-            String name = wordBtn[index].getText().toString();
-            handleButtonClick(url, name, buttonId);
-        } else if (buttonId == R.id.backBtn) {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
+    private View.OnClickListener click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int buttonId = v.getId();
+            int index = Arrays.asList(btnId).indexOf(buttonId);
+            if (index != -1) {
+                String url = "http://192.168.64.2:3000/btn" + (index + 1);
+                String name = wordBtn[index].getText().toString();
+                handleButtonClick(url, name, buttonId);
+            } else if (buttonId == R.id.backBtn) {
+                Intent intent = new Intent(WordListActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         }
-    }
+    };
 
     private void handleButtonClick(String url, String name, int buttonId) {
         new Thread(() -> {
@@ -68,21 +72,21 @@ public class WordListActivity extends AppCompatActivity implements View.OnClickL
                 if (response.isSuccessful()) {
                     final String responseData = response.body().string();
                     runOnUiThread(() -> {
-                        Log.d(TAG, url);
                         try {
-                            String word = new JSONObject(responseData).getString("word");
-                            String voice = new JSONObject(responseData).getString("voice");
-                            String detail = new JSONObject(responseData).getString("detail");
-                            String videoUrl = new JSONObject(responseData).getString("videoUrl");
+                            JSONObject jsonObject = new JSONArray(responseData).getJSONObject(0);
+                            String word = jsonObject.getString("word");
+                            String voice = jsonObject.getString("voice");
+                            String detail = jsonObject.getString("detail");
+                            String pronunciation_url = jsonObject.getString("pronunciation_url");
 
-                            Log.d(TAG, videoUrl);
                             Intent intent = new Intent(WordListActivity.this, WordDetail.class);
                             intent.putExtra("buttonId", buttonId);
                             intent.putExtra("word", word);
                             intent.putExtra("detail", detail);
                             intent.putExtra("voice", voice);
-                            intent.putExtra("videoData", videoUrl);
+                            intent.putExtra("pronunciation_url", pronunciation_url);
                             startActivity(intent);
+                            Log.d(TAG, pronunciation_url);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
